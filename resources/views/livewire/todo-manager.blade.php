3fca4e8ee359dad71808227c2d6b1db4d05b7022
KirShet <kirshet2000@gmail.com>
2024-10-20 19:35:58 +0600

    Add deleteTodo functionality and button for removing todos by ID.

--- resources/views/livewire/todo-manager.blade.php
+++ resources/views/livewire/todo-manager.blade.php
@@ -16,10 +16,18 @@ new class extends Component {
         Auth::user()
             ->todos()
             ->create([
-                'name' => $this->todoName,
+                'name' => $this->pull('todoName'),
         ]);
     }
 
+    public function deleteTodo($id)
+    {
+        Auth::user()
+        ->todos()
+        ->where('id', $id)
+        ->delete();
+    }
+    
     public function with()
     {
         return [
@@ -35,11 +43,11 @@ new class extends Component {
         <x-input-error :messages="$errors->get('todoName')" class="mt-2" />
     </form>
     @foreach ($todos as $todo)
-        <div wire:key='{{ $todo->id }}'>
+        <div wire:key='{{ $todo->id }}' class="flex items-center space-x-4 space-y-2">
             <div>
                 {{$todo->name}}
             </div>
-            <button wire:click='delete({{$todo->id}})'
+            <button wire:click='deleteTodo({{ $todo->id }})'
                 class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700">Delete</button>
         </div>
     @endforeach
